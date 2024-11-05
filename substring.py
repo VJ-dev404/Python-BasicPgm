c595e9d48cb0545f16489fae3d82077bbf965bb4
VJdev-404 <vishwajeetrajbhar01@gmail.com>
2024-11-05 16:52:48 +0530

    Substring in String

---
+++ substring.py
@@ -0,0 +1,12 @@
+# Program to check Substring in String
+text = "Geeks welcome to the Geek Kingdom!"
+
+if "Geek" in text:
+    print("Substring found!")
+else:
+    print("Substring not found!")
+
+if "For" in text:
+    print("Substring found!")
+else:
+    print("Substring not found!")
